c1576c0f591c665ad1d26e8e1d9e3baf283634e6
Samik Maniar <samik.maniar@thegatewaycorp.com>
2021-01-22 12:56:51 +0530

    test changes

--- src/app/employee-listing/employee-listing.component.ts
+++ src/app/employee-listing/employee-listing.component.ts
@@ -1,22 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { EmployeeDataService } from '../employee-data.service';
 @Component({
   selector: 'app-employee-listing',
   templateUrl: './employee-listing.component.html',
   styleUrls: ['./employee-listing.component.css']
 })
 export class EmployeeListingComponent implements OnInit {
-  public employees = [
-    { id: "1", employee_name: "Gopal Joshi", Age: 37, Gender: "Male" },
-    { id: "2", employee_name: "Samik Maniar", Age: 38, Gender: "Male" },
-    { id: "3", employee_name: "Vimal Chauhan", Age: 35, Gender: "Male" },
-    { id: "4", employee_name: "Vivek Joshi", Age: 22, Gender: "Male" },
-    { id: "5", employee_name: "Rashid Shaikh", Age: 32, Gender: "Male" },
-  ]
-
-  constructor() { }
+  public employees = []
+  constructor(private employeeService: EmployeeDataService) { }
 
   ngOnInit(): void {
+    this.employees = this.employeeService.getEmployees();
   }
 
 }
